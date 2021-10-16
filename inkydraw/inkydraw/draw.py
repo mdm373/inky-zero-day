2271b170ec8af9c6c12583bf01f76d904d1ababb
Mark Mayer <mdm373@gmail.com>
2021-10-16 12:33:09 -0400

    clean

--- inkydraw/inkydraw/draw.py
+++ inkydraw/inkydraw/draw.py
@@ -1,7 +1,33 @@
 from os import path
+from inky.auto import auto
+from PIL import Image
+import time
 
 def draw(requestPath):
     file_path = path.relpath(requestPath)
+    inky_display = auto(ask_user=False, verbose=True)
     with open(file_path) as f:
         lines = f.readlines()
         print(lines)
+
+    colours = (inky_display.RED, inky_display.BLACK, inky_display.WHITE)
+    colour_names = (inky_display.colour, "black", "white")
+    img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+
+    # Loop through the specified number of cycles and completely
+    # fill the display with each colour in turn.
+
+    for i in range(3):
+        print("Cleaning cycle %i\n" % (i + 1))
+        for j, c in enumerate(colours):
+            print("- updating with %s" % colour_names[j])
+            inky_display.set_border(c)
+            for x in range(inky_display.WIDTH):
+                for y in range(inky_display.HEIGHT):
+                    img.putpixel((x, y), c)
+            inky_display.set_image(img)
+            inky_display.show()
+            time.sleep(1)
+        print("\n")
+
+    print("Cleaning complete!")
