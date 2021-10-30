098513110b3f0439e0930b4264d420aac1faf290
Mark Mayer <mdm373@gmail.com>
2021-10-30 01:28:46 -0400

    bat value updates

--- scripts/update_battery.sh
+++ scripts/update_battery.sh
@@ -6,7 +6,7 @@ while ! [ -s ./.temp/battery.txt ]; do
     echo "get battery" | nc -q 0 localhost 8423 > ./.temp/battery.txt
     echo "..."
     sleep 1 # throttle the check
-    COUNT=${COUNT}+1
+    COUNT=$((COUNT+1))
     if test ${COUNT} -eq 3 ; then
       echo "failed to update battery"
       exit 1
