46391d869ded105b2a9678077d979ae12a17fb52
Mark Mayer <mdm373@gmail.com>
2021-11-04 01:50:05 -0400

    wake fix

--- scripts/schedule_reboot.sh
+++ scripts/schedule_reboot.sh
@@ -1,4 +1,4 @@
 echo "rtc_pi2rtc" | nc -q 0 localhost 8423
 
 wake_at=$(date -d "+4 hour" -u +"%Y-%m-%dT%H:%M:%SZ")
-echo "rtc_alarm_set ${wake_at}" | nc -q 0 localhost 8423
+echo "rtc_alarm_set ${wake_at} 1" | nc -q 0 localhost 8423
