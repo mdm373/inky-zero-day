600e441921b0e0c6381c5e8dbe2d42e928492188
Mark Mayer <mdm373@gmail.com>
2021-11-04 01:18:17 -0400

    install update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,11 @@
 python3 -m pip install Pillow
-python3 -m pip install  google-api-python-client==1.7.9
-python3 -m pip install  google-auth-httplib2==0.0.3
-python3 -m pip install  google-auth-oauthlib==0.4.0
+python3 -m pip install google-api-python-client==1.7.9
+python3 -m pip install google-auth-httplib2==0.0.3
+python3 -m pip install google-auth-oauthlib==0.4.0
 python3 -m pip install inky
-python3 -m pip install  python-dotenv
+python3 -m pip install python-dotenv
 python3 -m pip install python-dateutil
+python3 -m pip install flask
+python3 -m pip install Flask-RESTful
 mkdir .temp
 cp -n .env_shadow .env
